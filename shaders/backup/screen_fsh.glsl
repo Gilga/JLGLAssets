d2b4f59044172764e4585a57e82157b21596a5dd
Gilga <gilgatx@gmail.com>
2019-06-15 10:18:45 +0200

    *

--- shaders/backup/screen_fsh.glsl
+++
@@ -1,8 +0,0 @@
-#version 460
-layout(location = 0) uniform sampler2D iTexture;
-layout(location = 0) in vec2 texCoord;
-layout(location = 0) out vec4 outColor;
-
-void main() {
-  outColor = texture(iTexture, texCoord);
-}
